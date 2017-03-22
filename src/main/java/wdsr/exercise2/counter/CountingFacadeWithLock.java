4a5dc5db47b0b36aec04b49113d6d3afd627e05a
Marcin <m.wygledowski92@gmail.com>
2017-03-22 22:36:27 +0100

    couter was done

--- src/main/java/wdsr/exercise2/counter/CountingFacadeWithLock.java
+++ src/main/java/wdsr/exercise2/counter/CountingFacadeWithLock.java
@@ -1,24 +1,40 @@
 package wdsr.exercise2.counter;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by Marek on 05.03.2016.
  * 
- * Task: use {@see java.util.concurrent.locks.Lock} to make CountingFacadeWithLockTest pass. 
+ * Task: use {@see java.util.concurrent.locks.Lock} to make
+ * CountingFacadeWithLockTest pass.
  */
 public class CountingFacadeWithLock implements CountingFacade {
 	private final BusinessService businessService;
-	
+
 	private int invocationCounter;
-	
+
+	private Lock lock = new ReentrantLock();
+
 	public CountingFacadeWithLock(BusinessService businessService) {
 		this.businessService = businessService;
 	}
-		
+
 	public void countAndInvoke() {
-		invocationCounter++;
+		
+		try {
+			if (lock.tryLock(1, TimeUnit.SECONDS)) {
+				invocationCounter++;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
 		businessService.executeAction();
 	}
-	
+
 	public int getCount() {
 		return invocationCounter;
 	}
--- src/main/java/wdsr/exercise2/counter/SimpleCountingFacade.java
+++ src/main/java/wdsr/exercise2/counter/SimpleCountingFacade.java
@@ -3,22 +3,25 @@ package wdsr.exercise2.counter;
 /**
  * Created by Marek on 05.03.2016.
  * 
- * Task: use 'synchronized' keyword in this file to make SimpleCountingFacadeTest pass.
+ * Task: use 'synchronized' keyword in this file to make
+ * SimpleCountingFacadeTest pass.
  */
 public class SimpleCountingFacade implements CountingFacade {
 	private final BusinessService businessService;
-	
+
 	private int invocationCounter;
-	
+
 	public SimpleCountingFacade(BusinessService businessService) {
 		this.businessService = businessService;
 	}
-	
+
 	public void countAndInvoke() {
-		invocationCounter++;
+		synchronized (this) {
+			invocationCounter++;
+		}
 		businessService.executeAction();
 	}
-	
+
 	public int getCount() {
 		return invocationCounter;
 	}
