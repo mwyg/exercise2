d53e12d778735e3e9bc54cee059b5e5840ec2f87
Marcin <m.wygledowski92@gmail.com>
2017-03-26 18:38:15 +0200

    implementation of buffer

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,55 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
- * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
+ * Task: implement Buffer interface without using any *Queue classes from
+ * java.util.concurrent package. Any combination of "synchronized", *Lock,
+ * *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+
+	final Lock lock = new ReentrantLock();
+	final Condition notFull = lock.newCondition();
+	final Condition notEmpty = lock.newCondition();
+
+	final Order[] orders = new Order[100];
+	int putptr, takeptr, count;
+
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		lock.lock();
+		try {
+			while (count == orders.length) {
+				notFull.await();
+			}
+			orders[putptr] = order;
+			if (++putptr == orders.length) {
+				putptr = 0;
+			}
+			++count;
+			notEmpty.signal();
+		} finally {
+			lock.unlock();
+		}
 	}
-	
+
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		lock.lock();
+		try {
+			while (count == 0) {
+				notEmpty.await();
+			}
+			Order order = orders[takeptr];
+			if (++takeptr == orders.length) {
+				takeptr = 0;
+			}
+			--count;
+			notFull.signal();
+			return order;
+		} finally {
+			lock.unlock();
+		}
 	}
 }
